4467df18e00d4a064d32b5320ee90104c5ca6915
Nweni Kyaw <nwenikyaw127@gmail.com>
2020-09-03 11:30:56 +0630

    first commit

---
+++ cli/src/components/Testing.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h1>{{title}}</h1>
+		<p>{{msg}}</p>
+		<p>This is Testing for child components.</p>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		props:{
+			msg:null
+		},
+		data (){
+			return{
+				title: 'Testing Vue'
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	
+</style>
